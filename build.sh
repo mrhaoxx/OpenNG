a6651228212a8e5a8dd3b00c1c308ac5c8ccf1b1
haoxingxing <mr.haoxx@gmail.com>
2023-07-18 21:30:41 +0800

    fix build

--- build.sh
+++ build.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
-echo Build C Libs
-git submodule update --init
-cd libs
-echo "Working Directory:" `pwd`
+# echo Build C Libs
+# git submodule update --init
+# cd libs
+# echo "Working Directory:" `pwd`
 
-# echo Build LuaJIT 
-# cd LuaJIT
-# make && sudo make install
-# sudo ln -sf luajit-2.1.0-beta3 /usr/local/bin/luajit
+# # echo Build LuaJIT 
+# # cd LuaJIT
+# # make && sudo make install
+# # sudo ln -sf luajit-2.1.0-beta3 /usr/local/bin/luajit
+# # cd ..
+
+# # echo Build Brotli
+# # cd brotli
+# # ./bootstrap && ./configure -static
+# # make && sudo make install
+# # cd ..
+
 # cd ..
-
-# echo Build Brotli
-# cd brotli
-# ./bootstrap && ./configure -static
-# make && sudo make install
-# cd ..
-
-cd ..
-git submodule deinit --all -f
-echo Complete!
-pwd
+# git submodule deinit --all -f
+# echo Complete!
+# pwd
 
 # echo Notice: You need to make sure that you have installed luajit in the running environment.
 
